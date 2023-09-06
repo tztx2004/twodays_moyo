f23f35ad048d07d85b9f3298f27ab1d08c91abd4
KimTaeHeon <kimtaeheon@KimTaeHeonui-MacBookPro.local>
2023-09-06 09:55:30 +0900

    img 경로 설정

--- next.config.js
+++ next.config.js
@@ -1,16 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
-  },
-}
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: true,
+	},
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
+		return config;
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '*.cloudfront.net',
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
