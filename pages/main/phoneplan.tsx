97bfb4e786f3d738d1d748315b9746d163ede94c
Flynn <tztx2004@gmail.com>
2023-09-06 16:34:06 +0900

    커밋

--- pages/main/phoneplan.tsx
+++ pages/main/phoneplan.tsx
@@ -1,9 +1,6 @@
-import Link from 'next/link'
-import {
-  AiFillQuestionCircle,
-  AiFillHeart,
-  AiFillStar,
-} from "react-icons/ai";
+import Link from "next/link";
+import { AiFillQuestionCircle, AiFillHeart, AiFillStar } from "react-icons/ai";
+import HoverTextBox from "../components/HoverTextBox/HoverTextBox";
 function PhonePlan() {
   // 데이터 장소 : data/data.json/ props.pageProps.planMetas[idx]
   return (
@@ -17,63 +14,101 @@ function PhonePlan() {
   );
 }
 
-function PlanCard(data:any) {
-  const clickHandler = (e:any)=>{
-    e.target.classList.toggle("on")
+function PlanCard(data: any) {
+  const clickHandler = (e:any) => {
+    e.target?.classList.toggle("on");
+  };
+  const hoverHandler = (e:Event)=>{
+    // e.target?.
   }
 
+  const tempData = [
+    {
+      id: 1,
+      company: "",
+      src: "./images/kTskylife.svg",
+      개통: true,
+      title: "",
+      dataPlan: "",
+      callPlan: "",
+      smsPlan: "",
+      price: 16200,
+      mno: "",
+      net: "",
+      mvnoRating: 4.4,
+      numOfSignup: 4346,
+    },
+    {
+      id: 2,
+      company: "",
+      src: "./images/이야기모바일.svg",
+      개통: false,
+      title: "",
+      dataPlan: "",
+      callPlan: "",
+      smsPlan: "",
+      price: 16200,
+      mno: "",
+      net: "",
+      mvnoRating: 4.4,
+      numOfSignup: 4346,
+    },
+  ];
+
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <img src="./images/kTskylife.svg" alt="kTskylife" />
-          </div>
-          <div>
-            <img src="./images/모요개통아이콘.svg" alt="모요개통아이콘" />
-          </div>
-        </div>
-        <div>
-          <h4>모두 충분 7GB+밀리의서재</h4>
-          <AiFillHeart size="24px" color="rgb(173 181 189/1)"
-          onClick={clickHandler}
-          />
-        </div>
-        <h3>
-          월 7GB + 1Mbps <AiFillQuestionCircle color="#dee2e6" />
-        </h3>
-        <ul>
-          <li>통화 무제한</li>
-          <li></li>
-          <li>문자 무제한</li>
-          <li></li>
-          <li>KT망</li>
-          <li></li>
-          <li>LTE</li>
-        </ul>
-        <div>
+    <>
+      {tempData.map((x, i) => (
+        <div key={x.id}>
           <div>
             <div>
-              월 16,200원
-              <AiFillQuestionCircle color="#dee2e6" />
+              <div>
+                <img src={x.src} alt="kTskylife" />
+              </div>
+              <div>
+                {x.개통 && <img src="./images/모요개통아이콘.svg" alt="모요개통아이콘" />}
+              </div>
             </div>
-            <p>
-              <AiFillStar color="rgb(252 196 25/1)" /> 4.4 | 00명이 선택
-            </p>
-          </div>
-          <div>
-            <Link href={"./pay-detail/1"}>
-
-            <button>자세히 보기</button>
-            </Link>
+            <div>
+              <h4>모두 충분 7GB+밀리의서재</h4>
+              <AiFillHeart
+                size="24px"
+                color="rgb(173 181 189/1)"
+                onClick={clickHandler}
+              />
+            </div>
+            <h3>
+              월 7GB + 1Mbps <AiFillQuestionCircle color="#dee2e6" />
+            </h3>
+            <ul>
+              <li>통화 무제한</li>
+              <li></li>
+              <li>문자 무제한</li>
+              <li></li>
+              <li>KT망</li>
+              <li></li>
+              <li>LTE</li>
+            </ul>
+            <div>
+              <div>
+                <div>
+                  월 16,200원
+                  <AiFillQuestionCircle color="#dee2e6" />
+                </div>
+                <p>
+                  <AiFillStar color="rgb(252 196 25/1)" /> 4.4 | 00명이 선택
+                </p>
+              </div>
+              <div>
+                <Link href={`./pay-detail/${x.id}`}>
+                  <button>자세히 보기</button>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
-
-
-
 
 export default PhonePlan;
