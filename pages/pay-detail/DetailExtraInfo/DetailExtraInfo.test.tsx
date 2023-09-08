7369b348d005bfa41cdbe531c4a71588d7efbc1b
KimTaeHeon <kimtaeheon@KimTaeHeonui-MacBookPro.local>
2023-09-08 17:17:05 +0900

    DetailExtraInfo 테스트코드 세분화

--- pages/pay-detail/DetailExtraInfo/DetailExtraInfo.test.tsx
+++ pages/pay-detail/DetailExtraInfo/DetailExtraInfo.test.tsx
@@ -36,17 +36,33 @@ describe('DetailExtraInfo', () => {
     expect(text).toBeInTheDocument();
   });
 
-  it('기본 제공 초과 시 요금에 대한 텍스트가 존재합니다.', () => {
+  it('영상 통화를 가지고 있습니다.', () => {
     const videoCall = screen.getByText(/영상 통화/);
-    const extraCall = screen.getByText(/부가 통화/);
-    const longMsg = screen.getByText('긴 문자');
-    const imgMsg = screen.getByText(/사진 포함 긴 문자/);
-    const videoMsg = screen.getByText(/영상 포함 긴 문자/);
 
     expect(videoCall).toBeInTheDocument();
+  });
+
+  it('부가 통화를 가지고 있습니다.', () => {
+    const extraCall = screen.getByText(/부가 통화/);
+
     expect(extraCall).toBeInTheDocument();
+  });
+
+  it('긴 문자를 가지고 있습니다.', () => {
+    const longMsg = screen.getByText('긴 문자');
+
     expect(longMsg).toBeInTheDocument();
+  });
+
+  it('사진 포함 긴 문자를 가지고 있습니다.', () => {
+    const imgMsg = screen.getByText(/사진 포함 긴 문자/);
+
     expect(imgMsg).toBeInTheDocument();
+  });
+
+  it('영상 포함 긴 문자를 가지고 있습니다.', () => {
+    const videoMsg = screen.getByText(/영상 포함 긴 문자/);
+
     expect(videoMsg).toBeInTheDocument();
   });
 });
