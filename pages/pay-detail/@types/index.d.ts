787200d27ca26dd2b45921160e4bad8957a5661e
KimTaeHeon <kimtaeheon@KimTaeHeonui-MacBookPro.local>
2023-09-12 15:26:00 +0900

    pay-detail 컴포넌트 types 폴더 생성

---
+++ pages/pay-detail/@types/index.d.ts
@@ -0,0 +1,39 @@
+interface Data {
+  numberOfUser: number;
+  additional_service: AdditionalService;
+  plan: Plan;
+}
+
+interface AdditionalService {
+  isSupported: string[];
+  isUnsupported: string[];
+}
+
+interface Plan {
+  plan_id: number;
+  carrier: string;
+  plan_title: string;
+  monthly_data: number;
+  daily_data: number;
+  postExhaustedDataSpeed: number;
+  requirement: string;
+  contract_period: number;
+  eSIM: number;
+  sim_delivery: number;
+  nfc_delivery: number;
+  data_sharing: number;
+  roaming: number;
+  micropayment: number;
+  authentication_method: string;
+  underage_registration: number;
+  foreigner_registration: number;
+  transfer_charge: number;
+  voice: string;
+  SMS: string;
+  network: string;
+  original_price: number;
+  discount_period: number;
+  discounted_price: number;
+  parent_carrier: string;
+  addition_call: string;
+}
