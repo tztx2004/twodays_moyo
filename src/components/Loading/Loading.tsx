ba0c7fb8c091007a602ea6135a1208ddf3c0f9ae
KimTaeHeon <kimtaeheon@KimTaeHeonui-MacBookPro.local>
2023-09-12 15:36:46 +0900

    Loading 컴포넌트 CSS 완성

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,3 +1,39 @@
+import styled from 'styled-components';
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <WrapperBox>
+      <Spinner></Spinner>
+    </WrapperBox>
+  );
 }
+
+const WrapperBox = styled.div`
+  background-color: #000000;
+  opacity: 0.4;
+  width: 100vw;
+  height: 100vh;
+  z-index: 9999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Spinner = styled.div`
+  border: 8px solid white;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  z-index: 99999;
+  border-right-color: transparent;
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
