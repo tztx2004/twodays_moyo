7500a4feacfcd42c22f6f4b85985137fdb9f2d3a
KimTaeHeon <kimtaeheon@KimTaeHeonui-MacBookPro.local>
2023-09-13 14:54:35 +0900

    test 폴더 삭제

--- pages/pay-detail/DetailTitle/DetailTitle.test.tsx
+++ pages/pay-detail/DetailTitle/DetailTitle.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import DetailTitle from './DetailTitle';
+import DetailTitle from '../DetailTitle';
 
 describe('DetailTitle', () => {
   beforeEach(() => {
