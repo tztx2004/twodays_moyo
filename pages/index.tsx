4923f14779aaeead490f98b590a01dd16034bed3
Flynn <tztx2004@gmail.com>
2023-09-13 14:13:24 +0900

    도커 올리기 전

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home({ data }: Idata) {
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const pageNumber = context.query.page || 1;
-  const res = await fetch(`http://172.30.1.68:3000/plans?page=${pageNumber}&items-per-page=5`);
+  const res = await fetch(`http://172.30.1.68:3000/plans?page=${pageNumber}&items-per-page=50`);
   const data = await res.json();
 
   return { props: { data } };
